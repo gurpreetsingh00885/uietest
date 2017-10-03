94fdc773e707cdcae789f142129d5d6d4501aa7e
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-10-04 01:47:29 +0530

    Corrected Time zone

--- uietest/settings.py
+++ uietest/settings.py
@@ -132,6 +132,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+TIME_ZONE = "Asia/Kolkata"
+
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
